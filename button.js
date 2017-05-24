06aee07d1ab67f65abd892cb6cc3b10d9bf1aeb0
jpb249 <jpb249@nau.edu>
2017-05-24 15:14:22 -0700

    Add files via upload

--- button.js
+++ button.js
@@ -1,26 +1,35 @@
+// Global variables used to track clicks and if the game is done
 var timesClicked = 0;
 var gameDone = false;
 
+// Runs when the button is clicked
 function buttonPress()
 {
 
+    // If the game is over, do nothing
     if (gameDone == true)
     {
         return;
     }
 
+    // Increment the click counter
     timesClicked++;
 
+    // Update the click display
     document.getElementById("clickDisplay").innerHTML = "Times clicked: " + timesClicked;
 
+    // If the click counter is above 30
     if (timesClicked > 30)
     {
 
+        // If it is an even number, move the button to the right
         if (timesClicked % 2 == 0)
         {
             document.getElementById("gameButton").style.marginLeft = 400;
             document.getElementById("gameButton").style.marginRight = 0;
         }
+
+        // If it is an odd number, move the button to the left
         else
         {
             document.getElementById("gameButton").style.marginRight = 400;
@@ -29,13 +38,20 @@ function buttonPress()
 
     }
 
+    // If the click counter is above 60
     if (timesClicked > 60)
     {
+
+        // If the click number is a multiple of three, move button to top
         if (timesClicked % 3 == 0)
         {
+
             document.getElementById("gameButton").style.marginTop = 200;
             document.getElementById("gameButton").style.marginBottom = 0;
+
         }
+
+        // If it is not a multiple of three, move button to bottom
         else
         {
             document.getElementById("gameButton").style.marginBottom = 200;
@@ -44,12 +60,14 @@ function buttonPress()
 
     }
 
+    // If the click counter is 72, change button and hidden text
     if (timesClicked == 72)
     {
         document.getElementById("gameButton").innerHTML = "Don't Click Me!!!";
         document.getElementById("hiddenP").innerHTML = "Click me instead";
     }
 
+    // If the click counter is 73
     if (timesClicked == 73)
     {
         timesClicked = 0;
@@ -59,6 +77,7 @@ function buttonPress()
         document.getElementById("gameButton").style.marginTop = 0;
         document.getElementById("gameButton").style.marginBottom = 0;
         document.getElementById("gameButton").innerHTML = "Click Me!";
+        document.getElementById("hiddenP").innerHTML = "";
     }
 
     if (timesClicked == 100)
@@ -81,6 +100,7 @@ function buttonPress()
         document.getElementById("gameButton").style.marginTop = 0;
         document.getElementById("gameButton").style.marginBottom = 0;
         document.getElementById("gameButton").innerHTML = "Click Me!";
+        document.getElementById("hiddenP").innerHTML = "";
     }
 
     if (timesClicked == 200)
@@ -91,6 +111,7 @@ function buttonPress()
 
 }
 
+// Runs when the hidden text is clicked
 function hiddenClick()
 {
     timesClicked++;
